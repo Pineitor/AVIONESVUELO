82c50af1ff92e45a7b2b44d256c8402c18a1f7de
Nicolas Pino Flores <nic.pinof@duocuc.cl>
2022-06-09 02:07:04 -0400

    Update VAL.py

--- VAL.py
+++ VAL.py
@@ -24,3 +24,50 @@ while initFlag == True:
         while opmenu < 1 or opmenu > 5:
             print("Opción seleccionada no válida")
             opmenu = int(input("Vuelva a ingresar una opción: "))
+
+        if opmenu == 1:
+            fn.verAsientos(listaAsientos)
+            opVer = int(input(""))
+            if opVer == 1:
+                reservaVuelo = fn.comprarAsiento(listaAsientos)
+                if reservaVuelo[0] >= 1 and reservaVuelo[0]<31:
+                    precio=AN
+                else:
+                    precio=AV
+                datos = fn.validarDatos()
+                if datos[3] == 3:
+                    precioFinal = fn.dsctoDuoc(precio,15)
+                else:
+                    precioFinal = precio
+                print(f"Sub total: ${precio}")
+                print(f"Dscto Banco Duoc: 15%")
+                print(f"Total a pagar: ${precioFinal}")
+            if opVer == 2:
+                initFlag == True
+
+        if opmenu == 2:
+            print("Asientos con X ya se encuentran reservados.")
+            print(listaAsientos)
+            reservaVuelo = fn.comprarAsiento(listaAsientos)
+            if reservaVuelo[0] >= 1 and reservaVuelo[0]<31:
+                precio=AN
+            else:
+                precio=AV
+            datos = fn.validarDatos()
+            if datos[3] == 3:
+                precioFinal = fn.dsctoDuoc(precio,15)
+            else:
+                precioFinal = precio
+            print(f"Total a pagar: ${precioFinal}")
+
+        if opmenu == 3:
+            fn.anularVuelo(reservaVuelo,listaAsientos)
+
+        if opmenu == 4:
+            fn.modificarPasajero(datos,reservaVuelo)
+
+        if opmenu == 5:
+            initFlag == False
+            break
+    except:
+        print("Error de datos. Regresando al menú principal.")
